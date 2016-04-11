a42bfce7c9e84fa7d7eb355aa2c05b5ac4c124ee
Andrew Baik <and.baik@gmail.com>
2016-04-11 14:43:04 -0400

    basic crud for todo's complete

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
 export class App {
-  message = 'Welcome to Aurelia!';
+
+  todo = {
+    desc: '',
+    date: '',
+    completed: false
+  };
+
+  todos = [];
+
+  addTodo() {
+    this.todos.push({
+      desc: this.todo.desc,
+      date: this.todo.date,
+      completed: false
+    });
+    this.todo.desc = '';
+    this.todo.date = '';
+    this.todo.completed = false;
+  }
+
+  deleteTodo(todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
+  }
 }
